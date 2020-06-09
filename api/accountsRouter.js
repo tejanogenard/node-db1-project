f850aaeb15276dda7a56a80596e7c00a64984771
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-06-09 12:57:57 -0700

    added accountsRouter file

---
+++ api/accountsRouter.js
@@ -0,0 +1,10 @@
+express = require("express")
+
+const knex = ("../data/dbConfig.js")
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    
+})
+module.exports = router; 
--- api/server.js
+++ api/server.js
@@ -4,6 +4,11 @@ const db = require("../data/dbConfig.js");
 
 const server = express();
 
+const accountRouter = require("./accountsRouter.js")
+
 server.use(express.json());
 
+server.use("/accounts", accountRouter)
+
 module.exports = server;
+ 
